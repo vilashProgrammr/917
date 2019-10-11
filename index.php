9bcdbd98540e37aee7a05618a1f4054b9baa278c
candidate <candidate@programmr.com>
2019-10-11 14:26:26 +0000

    FINISH TEST

--- index.php
+++ index.php
@@ -46,7 +46,7 @@ function delete_id(id)
 	<div id="content">
     <table align="center">
     <tr>
-    <th colspan="5"><a href="add_data.php">add data here.</a></th>
+    <th colspan="5"><a href="add_data.php">add data heres adasd.</a></th>
     </tr>
     <th>First Name</th>
     <th>Last Name</th>
